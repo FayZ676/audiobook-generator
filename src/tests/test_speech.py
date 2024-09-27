194a2cdafb705d92368357b06fdb2b870aac79c5
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2024-09-26 20:19:15 -0500

    Remove redundant docstring.

--- src/tests/test_speech.py
+++ src/tests/test_speech.py
@@ -2,6 +2,5 @@ from tta.models.speech import generate_speech
 
 
 def test_speech__returns_bytes():
-    """Test that the function returns bytes."""
     audio_data = generate_speech("Hello", "5ZvI0fBo2w7CxuiM9ObF")
     assert isinstance(audio_data, bytes), "The result should be of type bytes."
