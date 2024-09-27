95ebff8bad0b71b0424696cabfbcb15bc2802499
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2024-09-26 20:11:50 -0500

    Add working voice id

--- src/tests/test_speech.py
+++ src/tests/test_speech.py
@@ -3,5 +3,5 @@ from tta.models.speech import generate_speech
 
 def test_speech():
     """Test that the function returns bytes."""
-    audio_data = generate_speech("Hello", "")
+    audio_data = generate_speech("Hello", "5ZvI0fBo2w7CxuiM9ObF")
     assert isinstance(audio_data, bytes), "The result should be of type bytes."
