673a007af4875b87df3319d2b2e888ae85e0b549
Derrick <dstroverduncan@yahoo.com>
2025-02-20 13:04:32 -0600

    removing dialogue from quotations

--- tta/tta/text_handler.py
+++ tta/tta/text_handler.py
@@ -1,3 +1,5 @@
+import re
+
 def get_chunks(text: str, chunk_size: int) -> list[str]:
     if not text:
         return [""]
@@ -28,3 +30,12 @@ def get_chunks(text: str, chunk_size: int) -> list[str]:
         chunks.append("\n\n".join(current_chunk))
 
     return chunks
+
+
+def remove_dialogue(text: str) -> str:
+    # Pattern to get content inside single/double quotes
+    quote_pattern = r'(["\'])(?:(?=(\\?))\2.)*?\1'
+
+    text_without_dialogue = re.sub(quote_pattern, '', text)
+
+    return ' '.join(text_without_dialogue.split())
