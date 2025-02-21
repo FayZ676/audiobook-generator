d8e99b53a95e93f28b08fac30f9bcd358e5ba4ab
Derrick <dstroverduncan@yahoo.com>
2025-02-21 13:31:39 -0600

    unit tests

--- tta/tests/test_text_handler.py
+++ tta/tests/test_text_handler.py
@@ -1,4 +1,4 @@
-from tta.text_handler import get_chunks
+from tta.text_handler import get_chunks, remove_dialogue
 
 
 def test_basic_text_splitting():
@@ -29,3 +29,31 @@ def test_large_paragraphs():
     assert chunks[0] == "One two three four."
     assert chunks[1] == "Five six seven eight."
     assert chunks[2] == "Nine ten eleven twelve."
+
+def test_remove_dialogue_basic():
+    text = 'Dumbledore said, "Happiness can be found even in the darkest of times."'
+    assert remove_dialogue(text) == "Dumbledore said," 
+
+def test_remove_multiple_dialogues():
+    text = '"It does not do to dwell on dreams," Dumbledore said. "and forget to live."'
+    assert remove_dialogue(text) == "Dumbledore said."
+
+def test_remove_mixed_quotes():
+    text = "Snape whispered, 'Always.' and turned away."
+    assert remove_dialogue(text) == "Snape whispered, and turned away."
+
+def test_remove_nested_quotes():
+    text = """Harry said, "Hagrid told me, 'You're a wizard, Harry!'" """
+    assert remove_dialogue(text) == "Harry said,"
+
+def test_remove_quotes_with_punctuation():
+    text = 'Voldemort hissed, "There is no good and evil, only power."'
+    assert remove_dialogue(text) == "Voldemort hissed," 
+
+def test_remove_empty_quotes():
+    text = 'Hermione said, ""'
+    assert remove_dialogue(text) == "Hermione said," 
+
+def test_remove_dialogue_with_newlines():
+    text = 'Ron said, "Bloody hell!"\nHarry replied, "Brilliant!"'
+    assert remove_dialogue(text) == "Ron said, Harry replied,"
