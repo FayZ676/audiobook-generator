53738e7160f7e7f07add805642df4990039d4b9a
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2025-02-24 15:34:26 -0800

    Remove prints.

--- tta/tta/research/characterextraction/nlp_v2/main.py
+++ tta/tta/research/characterextraction/nlp_v2/main.py
@@ -2,31 +2,26 @@ import re
 from tta.ner import extract_entities, is_pos
 from tta.text_handler import remove_dialogue
 
-def main(text: str):
+
+def main(text: str) -> set[str]:
     paragraphs = text.split("\n\n")
-    paragraphs = [p for p in paragraphs if '"' not in p] #TODO: Investigate why we find "Harold". In a paragrapgh with no quotations
+    paragraphs = [
+        p for p in paragraphs if '"' not in p
+    ]  # TODO: Investigate why we find "Harold" and "Petunia". In a paragrapgh with no quotations
     paragraphs = [remove_dialogue(p) for p in paragraphs]
     names = []
     for paragraph in paragraphs:
         persons = extract_entities(paragraph, ["PERSON"])
-        print(f"Extracted persons: {persons}")  #FOR SEEING RESULTS CAN BE DELETED
         for person in persons:
             # USING REGULAR EXPRESSIONS SO THIS WORKS WITH COMPARE.PY
-            # TODO: Use Start and end for person entity *See extract_entities* 
-            match = re.search(r'\b' + re.escape(person.text) + r'\b', paragraph) 
+            # TODO: Use Start and end for person entity *See extract_entities*
+            match = re.search(r"\b" + re.escape(person.text) + r"\b", paragraph)
             if match:
                 start_index = match.start()
                 end_index = match.end()
-                surrounding_text = paragraph[max(0, start_index - 20):end_index + 20] #20 is an arbitrary number for characters before and after PERSON
-                print(f"Surrounding text for '{person.text}': {surrounding_text}")  #FOR SEEING RESULTS CAN BE DELETED
+                surrounding_text = paragraph[
+                    max(0, start_index - 20) : end_index + 20
+                ]  # 20 is an arbitrary number for characters before and after PERSON
                 if is_pos(surrounding_text, "VERB"):
-                    print(f"Found verb near '{person.text}'")  #FOR SEEING RESULTS CAN BE DELETED
                     names.append(person.text)
     return set(names)
-
-if __name__ == "__main__": #FOR SEEING RESULTS CAN BE DELETED
-    with open("c:/Users/Nathan/audiobook-generator-1/tta/tests/text/harrypotter-1.txt", encoding="utf-8") as f:
-        text = f.read()
-    
-    result = main(text)
-    print(result)
