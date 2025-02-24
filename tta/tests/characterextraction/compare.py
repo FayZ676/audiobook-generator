56f6c356a379d082aef04ef3a5ffbc91f93180fe
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2025-02-24 15:36:30 -0800

    Remove derrick code.

--- tta/tests/characterextraction/compare.py
+++ tta/tests/characterextraction/compare.py
@@ -1,4 +1,3 @@
-from tta.research.characterextraction import derrick as D
 from tta.research.characterextraction import nathan as N
 from tta.research.characterextraction.nlp_v2.main import main as M
 from tta.metrics import test_precision_recall
@@ -19,12 +18,6 @@ def nathan(text: str):
     print(f"Nathan -> PRECISION: {precision}, RECALL: {recall}")
 
 
-def derrick(text: str):
-    result = D.main(text)
-    precision, recall = test_precision_recall(result, EXPECTED)
-    print(f"Derrick -> PRECISION: {precision}, RECALL: {recall}")
-
-
 def main(text: str):
     result = M(text)
     precision, recall = test_precision_recall(result, EXPECTED)
@@ -32,10 +25,12 @@ def main(text: str):
 
 
 if __name__ == "__main__":
-    #CHANGE PATHING FOR TXT FILE
-    with open("c:/Users/Nathan/audiobook-generator-1/tta/tests/text/harrypotter-1.txt", encoding="utf-8") as f:
+    # CHANGE PATHING FOR TXT FILE
+    with open(
+        "c:/Users/Nathan/audiobook-generator-1/tta/tests/text/harrypotter-1.txt",
+        encoding="utf-8",
+    ) as f:
         text = f.read()
 
     nathan(text)
-    derrick(text)
     main(text)
