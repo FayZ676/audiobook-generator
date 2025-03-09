b61e3728f5966002d3e6f02d61fed0ee5d9e4a40
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2025-03-09 10:56:21 -0700

    Refactoring.

---
+++ tta/tests/research/test_characterextractionnlp.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from tta.research.characterextractionnlp import main as M
+from tta.metrics import test_precision_recall
+
+
+EXPECTED = {
+    "Professor McGonagall",
+    "Albus Dumbledore",
+    "Mrs. Dursley",
+    "Mr. Dursley",
+    "Hagrid",
+}
+
+
+def main(text: str):
+    result = M(text)
+    for r in result:
+        print(r)
+    precision, recall = test_precision_recall(result, EXPECTED)
+    print(f"Main -> PRECISION: {precision}, RECALL: {recall}")
+
+
+def get_text():
+    with open(Path.cwd() / "../text/harrypotter-1.txt", encoding="utf-8") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    main(get_text())
