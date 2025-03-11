95d23d1c64d87e42a9f57646fc5fb54fa8e05e88
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2025-03-11 13:28:06 -0700

    Add test for harry potter chapter 1 through 3.

--- tta/tests/test_character.py
+++ tta/tests/test_character.py
@@ -4,12 +4,12 @@ from tta.character import get_speakers
 from tta.metrics import precision_recall
 
 
-def get_text():
-    with open(Path.cwd() / "text/harrypotter-1.txt", encoding="utf-8") as f:
+def get_text(name: str):
+    with open(Path.cwd() / f"text/{name}", encoding="utf-8") as f:
         return f.read()
 
 
-def test_get_speakers():
+def test_get_speakers__hp_chapter_1():
     expected = {
         ("Professor McGonagall",),
         ("Albus Dumbledore",),
@@ -17,7 +17,24 @@ def test_get_speakers():
         ("Mr. Dursley",),
         ("Hagrid",),
     }
-    result = get_speakers(get_text())
+    result = get_speakers(get_text("harrypotter-1.txt"))
+    precision, recall = precision_recall(result, expected)
+    print(f"Main -> PRECISION: {precision}, RECALL: {recall}")
+    assert result == expected
+
+
+def test_get_speakers__hp_chapter_1_3():
+    expected = {
+        ("Professor McGonagall",),
+        ("Albus Dumbledore",),
+        ("Mrs. Dursley",),
+        ("Mr. Dursley",),
+        ("Hagrid",),
+        ("Uncle Vernon",),
+        ("Aunt Petunia",),
+        ("Dudley",),
+    }
+    result = get_speakers(get_text("harrypotter-1-3.txt"))
     precision, recall = precision_recall(result, expected)
     print(f"Main -> PRECISION: {precision}, RECALL: {recall}")
     assert result == expected
