aaab102b3d75c588c97af0f9569e7b9c46191f78
FayZ676 <faizififita1@gmail.com>
2025-04-13 12:09:14 -0700

    Add jim dale voice.

--- f5tts/main.py
+++ f5tts/main.py
@@ -6,10 +6,9 @@ def generate(text: str, voice: str):
     voice_value = next((v for v in voices if v.name.lower() == voice.lower()), None)
     if not voice:
         raise ValueError(f"Voice '{voice}' not found.")
-
     audio = infer(
-        ref_audio=voice_value.audio_path,
-        ref_text=voice_value.audio_transcript,
+        ref_audio=voice_value.audio_path,  # type: ignore
+        ref_text=voice_value.audio_transcript,  # type: ignore
         gen_text=text,
         output_path="output.wav",
         vocab_file="F5-TTS/src/f5_tts/infer/examples/vocab.txt",
@@ -22,6 +21,6 @@ def generate(text: str, voice: str):
 
 if __name__ == "__main__":
     generate(
-        "This may sound like Faizi, but it's actually not. This is an AI voice.",
-        "Faizi",
+        "Although this may sound like Jim, this is in fact a computer generated voice made to sound like him. Don't freak out, I'll still make your story sound fantastic.",
+        "Jim Dale",
     )
--- f5tts/voices/labels.py
+++ f5tts/voices/labels.py
@@ -6,6 +6,8 @@ from dataclasses import dataclass
 @dataclass
 class Voice:
     name: str
+    age: str
+    gender: str
     audio_path: str
     audio_transcript: str
 
@@ -13,7 +15,16 @@ class Voice:
 voices = [
     Voice(
         "Faizi",
+        "young",
+        "male",
         f"{Path(__file__).parent}/audios/faizi.mp3",
         "The little red fox jumped over the white picket fence and onto the crocodile that was sitting on the roof of the house. He didn't realize that there was a flamingo sitting up there with him.",
+    ),
+    Voice(
+        "Jim Dale",
+        "middle-aged",
+        "young",
+        f"{Path(__file__).parent}/audios/jim_dale.mp3",
+        "Standing on a golden perch behind the door was a decrepit looking bird that resembled a half plucked turkey. Harry stared at it and the bird looked balefully back, making it's gagging noise again."
     )
 ]
