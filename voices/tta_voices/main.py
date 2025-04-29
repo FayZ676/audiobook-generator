f0f08476d4615743782a842a0d6a3d11973e2899
FayZ676 <faizififita1@gmail.com>
2025-04-28 21:16:11 -0700

    Update todos.

--- voices/tta_voices/main.py
+++ voices/tta_voices/main.py
@@ -8,16 +8,19 @@ app = FastAPI()
 
 @app.get("/voices")
 def get_voices():
+    # TODO: Get all voices from dynamo db.
     return [Voice("Bob", "middle-aged", "male", "foo", "bar")]
 
 
 @app.get("/voices/{voice_id}")
 def get_voice(voice_id: str):
-    # TODO: Find voice
+    # TODO: Get voice from dynamo db.
     return Voice("Bob", "middle-aged", "male", "foo", "bar")
 
 
 @app.post("/voices")
-def add_voice(voice: Voice):
-    # TODO: Check that voice exists first.
+def add_voice(name: str, age: str, gender: str, audio: bytes, transcript: str):
+    # TODO: Upload audio to s3.
+    # TODO: Create voice type.
+    # TODO: Add voice to dynamo db.
     return
