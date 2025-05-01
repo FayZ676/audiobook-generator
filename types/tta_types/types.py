e7eef1057892370f92961da8304b2c8fa9e8b60d
FayZ676 <faizififita1@gmail.com>
2025-04-30 22:12:50 -0700

    Refactor to take list of voices.

--- types/tta_types/types.py
+++ types/tta_types/types.py
@@ -12,12 +12,6 @@ class Voice:
     audio_path: str
     audio_transcript: str
 
-    def audio_to_dict(self) -> dict[str, str]:
-        return {
-            "ref_audio": self.audio_path,
-            "ref_text": self.audio_transcript,
-        }
-
     def to_json_fileobject(self) -> BinaryIO:
         json_bytes = json.dumps(asdict(self), indent=4).encode("utf-8")
         file_obj = io.BytesIO(json_bytes)
