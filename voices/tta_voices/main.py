192a35ca10a3db4c2ceef554b0dc03fc699b148f
FayZ676 <faizififita1@gmail.com>
2025-04-30 21:18:02 -0700

    implement add voice.

--- voices/tta_voices/main.py
+++ voices/tta_voices/main.py
@@ -1,31 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 from tta_types.types import Voice
 
-from tta_voices.aws import DynamoDBClient, S3Client
+from tta_voices.aws import S3Client
 
 
 app = FastAPI()
 
 
-dynamo_db_client = DynamoDBClient()
 s3_client = S3Client()
 
 
-@app.get("/voices")
-def get_voices():
-    voices = dynamo_db_client.get_items("VoicesTable")
-    return [Voice(**voice) for voice in voices]
+# @app.get("/voices")
+# def get_voices():
+#     voices = s3_client.get_items("VoicesTable")
+#     return [Voice(**voice) for voice in voices]
 
 
-@app.get("/voices/{voice_id}")
-def get_voice(voice_id: str):
-    voice = dynamo_db_client.get_item("VoicesTable", {"voice_id": voice_id})
-    return Voice(**voice)
+# @app.get("/voices/{voice_id}")
+# def get_voice(voice_id: str):
+#     voice = s3_client.get_item("VoicesTable", {"voice_id": voice_id})
+#     return Voice(**voice)
 
 
 @app.post("/voices")
-def add_voice(name: str, age: str, gender: str, audio: bytes, transcript: str):
-    samples = s3_client.get_files("voices-audios-bucket")
-    print(samples)
+def add_voice(
+    name: str, age: str, gender: str, audio_transcript: str, audio_file: UploadFile
+):
+    if not audio_file.filename:
+        raise ValueError("Audio file with name is required")
+
+    path = s3_client.upload_fileobj(
+        "voices-audios-bucket", audio_file.filename, audio_file.file
+    )
+    s3_client.upload_fileobj(
+        "voices-metadata-bucket",
+        f"{name}.json",
+        Voice(
+            name=name,
+            age=age,
+            gender=gender,
+            audio_path=path,
+            audio_transcript=audio_transcript,
+        ).to_json_fileobject(),
+    )
     return
