dbda29f3e5d4dc5ef30f07a86b5447762410cca0
FayZ676 <faizififita1@gmail.com>
2025-05-01 21:39:43 -0700

    Implement saving script.

--- script/tta_script/main.py
+++ script/tta_script/main.py
@@ -1,20 +1,43 @@
+import io
+import json
+from dataclasses import asdict
+from typing import BinaryIO
+
 from tta_script.character.extract import get_speaker_details
 from tta_script.dialogue.extract import get_dialogue_details
+from tta_script.dialogue.types import DialogueDetails
 from tta_script.voices import assign_voices
 from tta_script.character.types import SpeakerDetails
 from tta_script.voices import SpeakerVoice
 
 from tta_types.types import Voice
 
+from tta_aws.s3 import S3Client
 
-def generate_script(text: str, voices: list[Voice], narrator_name: str):
+
+SCRIPT_RESULTS_BUCKET = "tta-script-results"
+
+
+def to_json_fileobject(
+    filename: str, dialogue_details: list[DialogueDetails]
+) -> BinaryIO:
+    json_bytes = json.dumps([asdict(d) for d in dialogue_details], indent=4).encode(
+        "utf-8"
+    )
+    file_obj = io.BytesIO(json_bytes)
+    file_obj.name = f"{filename}.json"
+    return file_obj
+
+
+def generate_script(title: str, text: str, voices: list[Voice], narrator_name: str):
     speaker_voices = assign_voices(get_speaker_details(text), voices=voices)
     narrator_voice = SpeakerVoice(
         SpeakerDetails(frozenset({"Narrator"}), "middle-aged", "male"),
         next(voice for voice in voices if voice.name == narrator_name),
     )
     dialogue_details = get_dialogue_details(text, speaker_voices, narrator_voice)
-    # TODO: Save script to S3.
+    file = to_json_fileobject(title, dialogue_details)
+    S3Client().upload_fileobj(SCRIPT_RESULTS_BUCKET, file.name, file)
 
 
 if __name__ == "__main__":
@@ -33,6 +56,7 @@ if __name__ == "__main__":
 
     voices: list[Voice] = get_voices_from_api()
     generate_script(
+        title="harrypotter-sample-tiny",
         text=get_text("harrypotter-sample-tiny.txt"),
         voices=voices,
         narrator_name="Jim Dale",
