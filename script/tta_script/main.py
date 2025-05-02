1372452e87aebc87983ae1289ef04b381acfef3a
FayZ676 <faizififita1@gmail.com>
2025-05-01 21:42:22 -0700

    Renaming.

--- script/tta_script/main.py
+++ script/tta_script/main.py
@@ -18,7 +18,7 @@ from tta_aws.s3 import S3Client
 SCRIPT_RESULTS_BUCKET = "tta-script-results"
 
 
-def to_json_fileobject(
+def _to_json_fileobject(
     filename: str, dialogue_details: list[DialogueDetails]
 ) -> BinaryIO:
     json_bytes = json.dumps([asdict(d) for d in dialogue_details], indent=4).encode(
@@ -36,28 +36,5 @@ def generate_script(title: str, text: str, voices: list[Voice], narrator_name: s
         next(voice for voice in voices if voice.name == narrator_name),
     )
     dialogue_details = get_dialogue_details(text, speaker_voices, narrator_voice)
-    file = to_json_fileobject(title, dialogue_details)
+    file = _to_json_fileobject(title, dialogue_details)
     S3Client().upload_fileobj(SCRIPT_RESULTS_BUCKET, file.name, file)
-
-
-if __name__ == "__main__":
-    import requests
-    from pathlib import Path
-
-    def get_text(filename: str) -> str:
-        with open(
-            f"{Path(__file__).parent}/../tests/text/{filename}", "r", encoding="utf-8"
-        ) as f:
-            return f.read()
-
-    def get_voices_from_api() -> list[Voice]:
-        response = requests.get("http://localhost:8000/voices")
-        return [Voice(**voice) for voice in response.json()]
-
-    voices: list[Voice] = get_voices_from_api()
-    generate_script(
-        title="harrypotter-sample-tiny",
-        text=get_text("harrypotter-sample-tiny.txt"),
-        voices=voices,
-        narrator_name="Jim Dale",
-    )
