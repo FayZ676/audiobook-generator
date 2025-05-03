4917a33b725a7fb256b38df8b50cf10d58eacea0
FayZ676 <faizififita1@gmail.com>
2025-05-03 15:27:24 -0700

    Build and store audio.

--- speech/tta_speech/main.py
+++ speech/tta_speech/main.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from pathlib import Path
 from pydub import AudioSegment
 from pydub.effects import normalize
@@ -5,12 +6,17 @@ from pydub.effects import normalize
 from fastapi import FastAPI
 
 from tta_speech.infer import infer
-from tta_speech.types import InferenceParams, InputData, SpeechRequest
+from tta_speech.types import InferenceParams, InputData, SpeechRequest, TextSegment
 
 from tta_types.types import Voice
+from tta_aws.s3 import S3Client
 
 
 app = FastAPI()
+
+
+s3 = S3Client()
+SPEECH_BUCKET = "tta-speech-results"
 
 
 def normalize_audio_volume(audio_path: str, headroom: float = 0.1) -> str:
@@ -26,7 +32,7 @@ def normalize_audio_volume(audio_path: str, headroom: float = 0.1) -> str:
 
 
 def _prepare_input(
-    request: list[SpeechRequest],
+    request: list[TextSegment],
     voices: list[Voice],
 ) -> InputData:
     def voice_to_dict(voice: Voice):
@@ -47,14 +53,26 @@ def _prepare_input(
     return InputData(text, voices_dict)
 
 
+def _build_audio(audio_segments: list[tuple[bytes, int | None]]) -> bytes:
+    combined = AudioSegment.empty()
+    for audio_bytes, sample_rate in audio_segments:
+        segment = AudioSegment.from_file(
+            BytesIO(audio_bytes), format="wav", frame_rate=sample_rate
+        )
+        combined += segment
+
+    output = BytesIO()
+    combined.export(output, format="mp3")
+    return output.getvalue()
+
+
 @app.post("/speech")
-def generate(request: list[SpeechRequest], voices: list[Voice]):
-    text_input = _prepare_input(request, voices)
-    return infer(
+def generate(request: SpeechRequest, voices: list[Voice]):
+    text_input = _prepare_input(request.text, voices)
+    result = infer(
         InferenceParams(
             gen_text=text_input.text,
             voices=text_input.voices,
-            output_path="data/output.wav",
             vocab_file=f"{Path(__file__).parent}/vocab.txt",
             vocoder_name="vocos",
             vocoder_local_path=f"{Path(__file__).parent}/vocos",
@@ -62,3 +80,6 @@ def generate(request: list[SpeechRequest], voices: list[Voice]):
             remove_silence=True,
         )
     )
+    audio = _build_audio([result])
+    s3.upload_fileobj(SPEECH_BUCKET, f"{request.title}.mp3", BytesIO(audio))
+    # TODO: Send notification to webhook.
