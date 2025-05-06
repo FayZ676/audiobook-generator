798e5dbfe33d733c020acec280b261c1d661b448
FayZ676 <faizififita1@gmail.com>
2025-05-06 15:12:31 -0700

    Merge voices service into main service.

--- service/tta_service/main.py
+++ service/tta_service/main.py
@@ -24,6 +24,8 @@ from fastapi.responses import StreamingResponse
 app = FastAPI()
 
 
+VOICES_AUDIOS_BUCKET = "tta-voices-audios"
+VOICES_METADATA_BUCKET = "tta-voices-metadata"
 SCRIPT_RESULTS_BUCKET = "tta-script-results"
 SPEECH_RESULTS_BUCKET = "tta-speech-results"
 TEXT_FILES_BUCKET = "tta-text-files"
@@ -99,6 +101,64 @@ def webhook(response: WebhookResponse):
             # TODO: Send script file to the client.
         case _:
             pass
+
+
+@app.get("/voices")
+def get_voices():
+    voices_metadata = s3_client.get_files(VOICES_METADATA_BUCKET)
+    voices: list[Voice] = []
+    for voice_metadata_key in voices_metadata:
+        file_content_bytes = s3_client.get_file(
+            VOICES_METADATA_BUCKET, str(voice_metadata_key)
+        )
+        voice_data = json.loads(file_content_bytes.decode("utf-8"))
+        voices.append(Voice(**voice_data))
+    return voices
+
+
+@app.get("/voices/{voice_id}")
+def get_voice(voice_name: str):
+    file_content_bytes = s3_client.get_file(
+        VOICES_METADATA_BUCKET, f"{voice_name}.json"
+    )
+    voice = json.loads(file_content_bytes.decode("utf-8"))
+    return Voice(**voice)
+
+
+@app.post("/voices")
+def add_voice(
+    name: str, age: str, gender: str, audio_transcript: str, audio_file: UploadFile
+):
+    def to_json_fileobject(voice: Voice) -> BinaryIO:
+        file_obj = io.BytesIO(voice.model_dump_json().encode("utf-8"))
+        file_obj.name = f"{voice.name}.json"
+        return file_obj
+
+    if not audio_file.filename:
+        raise ValueError("Audio file with name is required")
+
+    path = s3_client.upload_fileobj(
+        VOICES_AUDIOS_BUCKET, audio_file.filename, audio_file.file
+    )
+    s3_client.upload_fileobj(
+        VOICES_METADATA_BUCKET,
+        f"{name}.json",
+        to_json_fileobject(
+            Voice(
+                name=name,
+                age=age,
+                gender=gender,
+                audio_path=path,
+                audio_transcript=audio_transcript,
+            )
+        ),
+    )
+    return
+
+
+# TODO: Implement the update_voice function
+@app.patch("/voices/{voice_id}")
+def update_voice(name: str | None = None, age: str | None = None): ...
 
 
 def send_script_request(
