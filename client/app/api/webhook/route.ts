316daa929637d3a27a677e3bd338ef6319679db3
FayZ676 <faizififita1@gmail.com>
2025-05-07 15:07:59 -0700

    Parse webhook notification.

--- client/app/api/webhook/route.ts
+++ client/app/api/webhook/route.ts
@@ -1,9 +1,23 @@
+interface WebhookNotificationData {
+  filename: string;
+}
+
+interface WebhookNotification {
+  event: string;
+  job_id: string;
+  status: string;
+  data: WebhookNotificationData;
+}
+
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
-    // TODO: Extract script/speech filename.
-    // TODO: Fetch script/narration file from DB.
-    // TODO: Update UI components with the script/speech data.
+    const notification: WebhookNotification = await request.json();
+    if (notification.event == "script") {
+      // Revalidate script data.
+    }
+    if (notification.event == "narration") {
+      // Revalidate narration data.
+    }
   } catch (error) {
     console.error("Failed to parse JSON:", error);
     return new Response(JSON.stringify({ error: "Invalid JSON payload" }), {
