998d68c557b5f35cef457f9001308ad3386b0b29
FayZ676 <faizififita1@gmail.com>
2025-05-19 21:01:56 -0700

    Update status server side.

--- service/tta_service/main.py
+++ service/tta_service/main.py
@@ -204,23 +204,10 @@ def get_job_status(job_id: str):
     return AudiobookJob.model_validate(json.loads(job_status)).status
 
 
+# TODO: This should call the update_status function
 @app.patch("/job/status/{job_id}")
 def update_job_status(job_details: AudiobookJob):
-    if not s3_client.list_files(JOB_STATUS_BUCKET, job_details.job_id):
-        return HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Job ID {job_details.job_id} not found.",
-        )
-    data = s3_client.get_file(JOB_STATUS_BUCKET, job_details.job_id)
-    updated = AudiobookJob.model_validate(json.loads(data))
-    updated.status = job_details.status
-    file = io.BytesIO(updated.model_dump_json().encode("utf-8"))
-    file.name = f"{updated.job_id}.json"
-    s3_client.upload_fileobj(
-        JOB_STATUS_BUCKET,
-        file.name,
-        file,
-    )
+    update_status(job_details)
 
 
 @app.delete("/job/{job_id}")
@@ -244,6 +231,9 @@ async def webhook(response: WebhookResponse):
                 status="completed",
                 data=WebhookResponseResultData(filename=speech_data.filename),
             ).model_dump()
+            status = AudiobookJob(
+                job_id=response.user_id, status="Processing Narration"
+            )
         case "script":
             # TODO: Return the script data.
             script_data = ScriptResponse.model_validate(response.data)
@@ -252,11 +242,13 @@ async def webhook(response: WebhookResponse):
                 status="completed",
                 data=WebhookResponseResultData(filename=script_data.filename),
             ).model_dump()
+            status = AudiobookJob(job_id=response.user_id, status="Audiobook Complete")
         case _:
             return HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Invalid response type: {response_type}",
             )
+    update_status(status)
     pusher_client.trigger("job-channel", "job-completed", payload)
 
 
@@ -319,3 +311,21 @@ def send_async_request(url: str, payload: dict, headers: dict):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Failed to send script request: {str(e)}",
         )
+
+
+def update_status(job_details: AudiobookJob):
+    if not s3_client.list_files(JOB_STATUS_BUCKET, job_details.job_id):
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Job ID {job_details.job_id} not found.",
+        )
+    data = s3_client.get_file(JOB_STATUS_BUCKET, job_details.job_id)
+    updated = AudiobookJob.model_validate(json.loads(data))
+    updated.status = job_details.status
+    file = io.BytesIO(updated.model_dump_json().encode("utf-8"))
+    file.name = f"{updated.job_id}.json"
+    s3_client.upload_fileobj(
+        JOB_STATUS_BUCKET,
+        file.name,
+        file,
+    )
