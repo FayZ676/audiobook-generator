019589dfa14ca349b1d9005667b7e47b62f7e68f
FayZ676 <faizififita1@gmail.com>
2025-05-31 09:31:41 -0700

    subscribe to job update events.

--- client/app/components/JobStateClient.tsx
+++ client/app/components/JobStateClient.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
-import { use } from "react";
+import { use, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import pusherClient from "@/app/lib/pusher";
+
+import { handleRevalidateTag } from "@/app/actions/revalidate";
 
 import { AudiobookJob } from "../actions/job";
 
@@ -12,6 +17,23 @@ interface JobStateSectionProps {
 export default function JobStateClient({
   jobStatePromise,
 }: JobStateSectionProps) {
+  const router = useRouter();
+
   const jobState = use(jobStatePromise);
+
+  useEffect(() => {
+    const channel = pusherClient.subscribe("job-channel");
+
+    channel.bind("job-update", (data: {}) => {
+      handleRevalidateTag("job");
+      router.refresh();
+    });
+
+    return () => {
+      channel.unbind("job-update");
+      pusherClient.unsubscribe("job-channel");
+    };
+  }, []);
+
   return <div>{jobState?.script_status || jobState?.narration_status}</div>;
 }
