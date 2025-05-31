9940c7a7e15171b94ea3429b0d86410b9f1b5260
FayZ676 <faizififita1@gmail.com>
2025-05-31 09:23:04 -0700

    subscribe to narration events.

--- client/app/components/NarrationClient.tsx
+++ client/app/components/NarrationClient.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { use } from "react";
+import { use, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import pusherClient from "@/app/lib/pusher";
+
+import { handleRevalidateTag } from "@/app/actions/revalidate";
 
 import NarrationAudio from "./NarrationAudio";
 import NarrationButton from "./NarrationButton";
@@ -11,7 +16,24 @@ interface NarrationClientProps {
 export default function NarrationClient({
   narrationUrlPromise,
 }: NarrationClientProps) {
+  const router = useRouter();
+
   const narrationUrl = use(narrationUrlPromise);
+
+  useEffect(() => {
+    const channel = pusherClient.subscribe("narration-channel");
+
+    channel.bind("narration-complete", (data: {}) => {
+      handleRevalidateTag("narration");
+      router.refresh();
+    });
+
+    return () => {
+      channel.unbind("narration-completed");
+      pusherClient.unsubscribe("narration-channel");
+    };
+  }, []);
+
   return (
     <>
       {narrationUrl ? (
