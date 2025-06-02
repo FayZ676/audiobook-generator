90096f4573acb52331e70145429c7d13e67b8391
FayZ676 <faizififita1@gmail.com>
2025-06-01 20:12:27 -0700

    Renaming. Update component structure.

---
+++ client/app/components/ScriptClient.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from "react";
+import { use, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { handleRevalidateTag } from "@/app/actions/revalidate";
+
+import pusherClient from "@/app/lib/pusher";
+
+import { Script } from "../actions/script";
+
+import GenerateScriptForm from "./GenerateScriptForm";
+
+interface ScriptClientProps {
+  scriptPromise: Promise<Script | null>;
+}
+
+export default function ScriptClient({ scriptPromise }: ScriptClientProps) {
+  const router = useRouter();
+
+  const script = use(scriptPromise);
+
+  useEffect(() => {
+    const channel = pusherClient.subscribe("script-channel");
+
+    channel.bind("script-update", (data: {}) => {
+      console.log("Script updated");
+      handleRevalidateTag("script");
+      router.refresh();
+    });
+
+    return () => {
+      channel.unbind("script-update");
+      pusherClient.unsubscribe("script-channel");
+    };
+  }, []);
+
+  return (
+    <div>
+      {script ? (
+        script.map((scriptSegment, index) => {
+          // TODO: Don't use index as key.
+          return (
+            <div key={index} className="mb-4">
+              <p>
+                {scriptSegment.speaker.names[0]}: {scriptSegment.text}
+              </p>
+            </div>
+          );
+        })
+      ) : (
+        <GenerateScriptForm />
+      )}
+    </div>
+  );
+}
