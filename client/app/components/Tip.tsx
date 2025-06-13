478774d7e94fcdefa0477ef56b21f06500e2452d
FayZ676 <faizififita1@gmail.com>
2025-06-12 22:31:25 -0700

    Update styles.

--- client/app/components/Tip.tsx
+++ client/app/components/Tip.tsx
@@ -20,8 +20,19 @@ export default function Tip({ children, variant = "info" }: TipProps) {
     }
   };
 
+  const getClassName = () => {
+    switch (variant) {
+      case "info":
+        return "alert-info";
+      case "warning":
+        return "alert-warning";
+      case "success":
+        return "alert-success";
+    }
+  };
+
   return (
-    <div role="alert" className="alert">
+    <div role="alert" className={`alert ${getClassName()} alert-soft`}>
       <span>{getIcon()}</span>
       <span>{children}</span>
     </div>
