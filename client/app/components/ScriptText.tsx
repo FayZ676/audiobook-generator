ec95af14ea00e08793877693794f6a09b6b7f43e
FayZ676 <faizififita1@gmail.com>
2025-06-14 21:18:19 -0700

    Simplify.

--- client/app/components/ScriptText.tsx
+++ client/app/components/ScriptText.tsx
@@ -7,19 +7,16 @@ interface ScriptTextProps {
 }
 
 export default function ScriptText({ script }: ScriptTextProps) {
-
   return (
     <div className="h-[32rem] overflow-y-scroll bg-base-200 p-4 rounded">
       {script.map((scriptSegment, index) => {
         // TODO: Don't use index as key.
-        const tooltipContent = `${scriptSegment.voice_name} • ${scriptSegment.speaker.gender} • ${scriptSegment.speaker.age}`;
-
         return (
           <div key={index} className="mb-4">
             <p>
-              <span 
-                className="font-medium tooltip tooltip-right" 
-                data-tip={tooltipContent}
+              <span
+                className="font-medium tooltip tooltip-right"
+                data-tip={scriptSegment.voice_name}
               >
                 {scriptSegment.speaker.names[0]}
               </span>
