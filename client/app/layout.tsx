aac14334346c342334050d22eb840f1269f7eb70
FayZ676 <faizififita1@gmail.com>
2025-06-27 13:50:14 -0700

    Hide signup button since only sign in is supported.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
-              <SignUpButton />
+              {/* <SignUpButton /> */}
             </SignedOut>
             <SignedIn>
               <FeedbackForm />
