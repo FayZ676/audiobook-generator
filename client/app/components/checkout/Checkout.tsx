3076780bc0b692a7838c4fde26518407909c8f36
FayZ676 <faizififita1@gmail.com>
2025-06-30 20:08:19 -0700

    Swap ts-ignore for ts_expect-error

--- client/app/components/checkout/Checkout.tsx
+++ client/app/components/checkout/Checkout.tsx
@@ -17,7 +17,7 @@ export default function Checkout() {
     <div id="checkout">
       <EmbeddedCheckoutProvider
         stripe={stripePromise}
-        // @ts-ignore - This comes from Stripe's quickstart guide
+        // @ts-expect-error - This comes from Stripe's quickstart guide
         options={{ fetchClientSecret }}
       >
         <EmbeddedCheckout />
