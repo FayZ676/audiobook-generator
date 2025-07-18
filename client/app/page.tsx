054d05368536ca99b6160a8118f13416fa49180f
FayZ676 <faizififita1@gmail.com>
2025-07-17 17:48:36 -0700

    Remove unused import.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Play, Mic, Edit, Check } from "lucide-react";
+import { Play, Mic, Edit } from "lucide-react";
 import WaitlistForm from "./components/landing/WaitlistForm";
 import DemoAudio from "./components/landing/DemoAudio";
 
