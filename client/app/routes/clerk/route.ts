af6ee965a9e354e79bc64bbf54db06c582756c14
FayZ676 <faizififita1@gmail.com>
2025-07-23 14:19:36 -0700

    Add CLERK_WEBHOOK_SECRET to environment configuration and integrate svix for webhook validation

--- client/app/routes/clerk/route.ts
+++ client/app/routes/clerk/route.ts
@@ -1,14 +1,31 @@
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
 
 import { sendSubscriptionEvent } from "@/app/actions/subscription";
+import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+
+const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || ``;
+
+async function validateRequest(request: Request) {
+  const payloadString = await request.text();
+  const headerPayload: ReadonlyHeaders = await headers();
+
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
+  };
+  const wh = new Webhook(webhookSecret);
+  return wh.verify(payloadString, svixHeaders) as WebhookEvent;
+}
 
 export async function POST(request: Request) {
-  const payload: WebhookEvent = await request.json();
+  const payload = await validateRequest(request);
   console.log(payload);
   // @ts-expect-error TypeScript does not know about the subscriptionItem.active event.
   if (payload.type === "subscriptionItem.active") {
     try {
-      // TODO: Verify that the webhook request is from Clerk.
       // @ts-expect-error TypeScript does not know about the payload structure.
       await sendSubscriptionEvent(payload.data.payer.user_id, payload.type);
     } catch (error) {
