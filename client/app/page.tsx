3a6833dd911ab4061ec0dae98331b3bcb92471e9
FayZ676 <faizififita1@gmail.com>
2025-08-06 16:12:43 -0700

    Update landing page with visual.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,10 +6,13 @@ export default function Home() {
       {/* Hero Section */}
       <div className="hero bg-base-200 min-h-[70vh]">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div></div>
+          <img
+            src="dashboard.png"
+            className="max-w-2xl rounded-lg shadow-2xl"
+          />
           <div className="flex flex-col gap-8">
             <h1 className="text-5xl font-bold">
-              Your Stories Brought to Life<br></br>Exactly as You Imagined
+              Your Stories<br></br>Brought to Life<br></br>Exactly as Imagined
             </h1>
             <p className="text-xl font-medium">
               Produce multi-voice audiobooks with complete control. Customize
