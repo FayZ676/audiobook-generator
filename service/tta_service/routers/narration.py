73e26a78aad11a93c53d8845189f5574a505744f
FayZ676 <faizififita1@gmail.com>
2025-08-05 19:43:37 -0700

    Remove run sync

--- service/tta_service/routers/narration.py
+++ service/tta_service/routers/narration.py
@@ -96,7 +96,7 @@ async def send_narration_request(
     )
     # NOTE: Add /runsync endpoint when testing locally.
     send_async_request(
-        url=f"{SPEECH_API_URL}/runsync",
+        url=f"{SPEECH_API_URL}",
         payload={"input": request.model_dump()},
         headers={
             "Content-Type": "application/json",
