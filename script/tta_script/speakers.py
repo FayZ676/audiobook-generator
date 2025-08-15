aa24d2c7e69123d7ca9163f71788b235dc9b21f0
FayZ676 <faizififita1@gmail.com>
2025-08-15 18:28:38 -0400

    Refactoring

--- script/tta_script/speakers.py
+++ script/tta_script/speakers.py
@@ -10,32 +10,50 @@ def get_speakers(
 
     speakers: set[Speaker] = set()
     available_voices = voices.copy()
+
     for character in characters:
-        existing_speaker = next(
-            (
-                speaker
-                for speaker in previous_speakers
-                if any(name in speaker.character.names for name in character.names)
-            ),
-            None,
-        )
+        speaker = _get_speaker_for_character(character, previous_speakers)
+        if speaker:
+            speakers.add(speaker)
+            if speaker.voice in available_voices:
+                available_voices.remove(speaker.voice)
+            continue
 
-        if existing_speaker:
-            speakers.add(existing_speaker)
-            if existing_speaker.voice in available_voices:
-                available_voices.remove(existing_speaker.voice)
-        else:
-            matching_voices = [
-                voice
-                for voice in available_voices
-                if voice.age == character.age and voice.gender == character.gender
-            ]
-
-            if matching_voices:
-                selected_voice = matching_voices[0]
-                available_voices.remove(selected_voice)
-                speakers.add(Speaker(character=character, voice=selected_voice))
-            else:
-                speakers.add(Speaker(character=character, voice=narrator_voice))
+        voice = _assign_voice_to_character(character, available_voices, narrator_voice)
+        speakers.add(Speaker(character=character, voice=voice))
 
     return speakers
+
+
+def _get_character_voice(
+    character: Character, available_voices: list[Voice]
+) -> Voice | None:
+    return next(
+        (
+            voice
+            for voice in available_voices
+            if voice.age == character.age and voice.gender == character.gender
+        ),
+        None,
+    )
+
+
+def _get_speaker_for_character(character: Character, speakers: set[Speaker]):
+    return next(
+        (
+            speaker
+            for speaker in speakers
+            if any(name in speaker.character.names for name in character.names)
+        ),
+        None,
+    )
+
+
+def _assign_voice_to_character(
+    character: Character, available_voices: list[Voice], narrator_voice: Voice
+) -> Voice:
+    voice = _get_character_voice(character, available_voices)
+    if voice:
+        available_voices.remove(voice)
+        return voice
+    return narrator_voice
