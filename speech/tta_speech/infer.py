b093f41b1ff0de1c760e1b0a2285a38edd2a03d7
FayZ676 <faizififita1@gmail.com>
2025-09-08 16:28:14 -0700

    Update inference code.

--- speech/tta_speech/infer.py
+++ speech/tta_speech/infer.py
@@ -1,3 +1,9 @@
+# NOTE: Based on this example: https://github.com/nari-labs/dia/blob/main/example/voice_clone.py
+
+
+from dia.model import Dia
+
+
 class DiaTTS:
     def __init__(self):
         self.model = Dia.from_pretrained(
@@ -5,11 +11,16 @@ class DiaTTS:
         )
 
     def generate(
-        self, text: str, reference_audio: bytes, reference_transcript: str
-    ) -> bytes:
-        output = self.model.generate(
-            reference_transcript + text,
-            audio_prompt=reference_audio,
+        self,
+        text: str,
+        reference_audio_path: str,
+        reference_audio_transcript: str,
+        output_path: str,
+    ) -> None:
+        """Synthesize the provided `text` using `audio` for instant voice cloning and save output to a provided `output_path`."""
+        output_result = self.model.generate(
+            reference_audio_transcript + text,
+            audio_prompt=reference_audio_path,
             use_torch_compile=False,
             verbose=True,
             cfg_scale=4.0,
@@ -17,4 +28,4 @@ class DiaTTS:
             top_p=0.90,
             cfg_filter_top_k=50,
         )
-        return self.model.save_audio("voice_clone.mp3", output)
+        return self.model.save_audio(output_path, output_result)
