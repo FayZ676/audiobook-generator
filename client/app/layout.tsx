ca5f0400e80abea944eb59e0c833014d64186bef
FayZ676 <faizififita1@gmail.com>
2025-09-21 20:55:06 -0700

    Add speed insights package.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import {
   ClerkProvider,
   SignInButton,
@@ -48,6 +50,7 @@ export default function RootLayout({
             </SignedIn>
           </header>
           <main className="prose prose-lg max-w-none">{children}</main>
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
